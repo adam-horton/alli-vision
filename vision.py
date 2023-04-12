15d903f6992d6e3d3da55cdb50de9a924b2c2bac
Adam Horton <adam.horton@ufl.edu>
2023-04-12 14:29:09 -0400

    reorganized into class

--- vision.py
+++ vision.py
@@ -9,83 +9,90 @@ PORT = '8000'
 GATOR_BLUE_BGR = (165, 33, 0)
 GATOR_ORANGE_BGR = (22, 70, 250)
 
-hand_status = {
-                "RIGHT_HAND_RAISED" : False,
-                "LEFT_HAND_RAISED" : False,
-        }
+class VisionApp:
+        def __init__(self):
+                self.hand_status = {
+                                "RIGHT_HAND_RAISED" : False,
+                                "LEFT_HAND_RAISED" : False,
+                        }
+                self.app = Flask(__name__)
 
-app = Flask(__name__)
+        def addRoutes(self):
+                self.app.add_url_rule('/', view_func=self.index)
+                self.app.add_url_rule('/video_feed', view_func=self.video_feed)
+                self.app.add_url_rule('/status', view_func=self.status)
 
-@app.route('/')
-def index():
-        return render_template('index.html', status=hand_status)
+        def run(self):
+                self.app.run(host=HOST, port=PORT, debug=True, use_reloader=False)
 
-@app.route('/video_feed')
-def video_feed():
-        return Response(capture_and_detect(), mimetype = 'multipart/x-mixed-replace; boundary=frame')
+        def index(self):
+                return render_template('index.html')
 
-@app.route('/status', methods=['GET'])
-def status():
-        if hand_status["LEFT_HAND_RAISED"] and hand_status["RIGHT_HAND_RAISED"]:
-                status_text = 'Both Hands Raised'
-        elif hand_status["LEFT_HAND_RAISED"] and not hand_status["RIGHT_HAND_RAISED"]:
-                status_text = 'Left Hand Raised'
-        elif not hand_status["LEFT_HAND_RAISED"] and hand_status["RIGHT_HAND_RAISED"]:
-                status_text = 'Right Hand Raised'
-        else:
-                status_text = 'No Hands Raised'
-        return jsonify({"status": status_text})
+        def video_feed(self):
+                return Response(self.capture_and_detect(), mimetype = 'multipart/x-mixed-replace; boundary=frame')
 
-def capture_and_detect():
-        mp_drawing = mp.solutions.drawing_utils
-        mp_pose = mp.solutions.pose
+        def status(self):
+                if self.hand_status["LEFT_HAND_RAISED"] and self.hand_status["RIGHT_HAND_RAISED"]:
+                        status_text = 'Both Hands Raised'
+                elif self.hand_status["LEFT_HAND_RAISED"] and not self.hand_status["RIGHT_HAND_RAISED"]:
+                        status_text = 'Left Hand Raised'
+                elif not self.hand_status["LEFT_HAND_RAISED"] and self.hand_status["RIGHT_HAND_RAISED"]:
+                        status_text = 'Right Hand Raised'
+                else:
+                        status_text = 'No Hands Raised'
+                return jsonify({"status": status_text})
 
-        cap = cv2.VideoCapture(0)
-        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-                while True:
-                        #Read in one frame
-                        ret, frame = cap.read()
+        def capture_and_detect(self):
+                mp_drawing = mp.solutions.drawing_utils
+                mp_pose = mp.solutions.pose
 
-                        #Use mediapipe pose to process the image and determine landmarks
-                        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                        image.flags.writeable = False
-                        results = pose.process(image)
-                        image.flags.writeable = True
-                        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                cap = cv2.VideoCapture(0)
+                with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+                        while True:
+                                #Read in one frame
+                                ret, frame = cap.read()
 
-                        try:
-                                landmarks = results.pose_landmarks.landmark
-                                update_status(landmarks)
-                        except:
-                                pass
+                                #Use mediapipe pose to process the image and determine landmarks
+                                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                                image.flags.writeable = False
+                                results = pose.process(image)
+                                image.flags.writeable = True
+                                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-                        #Draw the landmarks on the image
-                        mp_drawing.draw_landmarks(
-                                image,
-                                results.pose_landmarks,
-                                mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=GATOR_ORANGE_BGR),
-                                mp_drawing.DrawingSpec(color=GATOR_BLUE_BGR)
-                                )
-                        
-                        #Send the frame to the live stream
-                        encodedImage = cv2.imencode('.jpg', image)[1]
-                        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+                                try:
+                                        landmarks = results.pose_landmarks.landmark
+                                        self.update_status(landmarks)
+                                except:
+                                        pass
 
-def update_status(landmarks):
-        global hand_status
-        mp_landmark = mp.solutions.pose.PoseLandmark
+                                #Draw the landmarks on the image
+                                mp_drawing.draw_landmarks(
+                                        image,
+                                        results.pose_landmarks,
+                                        mp_pose.POSE_CONNECTIONS,
+                                        mp_drawing.DrawingSpec(color=GATOR_ORANGE_BGR),
+                                        mp_drawing.DrawingSpec(color=GATOR_BLUE_BGR)
+                                        )
+                                
+                                #Send the frame to the live stream
+                                encodedImage = cv2.imencode('.jpg', image)[1]
+                                yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
 
-        if landmarks[mp_landmark.LEFT_SHOULDER.value].y > landmarks[mp_landmark.LEFT_WRIST.value].y:
-                hand_status["LEFT_HAND_RAISED"] = True
-        else:
-                hand_status["LEFT_HAND_RAISED"] = False
+        def update_status(self, landmarks):
+                mp_landmark = mp.solutions.pose.PoseLandmark
 
-        if landmarks[mp_landmark.RIGHT_SHOULDER.value].y > landmarks[mp_landmark.RIGHT_WRIST.value].y:
-                hand_status["RIGHT_HAND_RAISED"] = True
-        else:
-                hand_status["RIGHT_HAND_RAISED"] = False
+                if landmarks[mp_landmark.LEFT_SHOULDER.value].y > landmarks[mp_landmark.LEFT_WRIST.value].y:
+                        self.hand_status["LEFT_HAND_RAISED"] = True
+                else:
+                        self.hand_status["LEFT_HAND_RAISED"] = False
+
+                if landmarks[mp_landmark.RIGHT_SHOULDER.value].y > landmarks[mp_landmark.RIGHT_WRIST.value].y:
+                        self.hand_status["RIGHT_HAND_RAISED"] = True
+                else:
+                        self.hand_status["RIGHT_HAND_RAISED"] = False
 
 
 if __name__ == "__main__":
-        app.run(host=HOST, port=PORT, debug=True, use_reloader=False)
+        app = VisionApp()
+        app.addRoutes()
+        app.run()
